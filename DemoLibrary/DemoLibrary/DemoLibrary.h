623e1cfd96eed6a3ec9a3a8f40de0d82a0e73bef
Tinh <nguuyenvantinhfithou@gmail.com>
2021-04-26 14:54:50 +0700

    r

---
+++ DemoLibrary/DemoLibrary/DemoLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DemoLibrary.h
+//  DemoLibrary
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoLibrary.
+FOUNDATION_EXPORT double DemoLibraryVersionNumber;
+
+//! Project version string for DemoLibrary.
+FOUNDATION_EXPORT const unsigned char DemoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoLibrary/PublicHeader.h>
+
+
---
+++ DemoLibrary/DemoLibrary/DemoLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  DemoLibrary.swift
+//  DemoLibrary
+//
+//  Created by Nguyen Van Tinh on 4/26/21.
+//
+
+import Foundation
+
+public struct DemoLibrary{
+    public static func TestDemo(){
+        print("Da hien thi")
+    }
+}
